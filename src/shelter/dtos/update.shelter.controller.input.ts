ed9737bee166a04d3576566bdb7a891081391615
joaopaulocosta551 <kenocosta44@gmail.com>
2024-04-03 22:08:15 -0300

    Feat: Criada validações no shelter input

--- src/shelter/dtos/update.shelter.controller.input.ts
+++ src/shelter/dtos/update.shelter.controller.input.ts
@@ -1,6 +1,25 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumberString,
+  IsString,
+  Length,
+} from 'class-validator';
+
 export default class UpdateShelterControllerInput {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+  @IsNotEmpty()
+  @IsString()
+  @Length(10, 11)
   whatsapp: string;
+  @IsNotEmpty()
+  @IsString()
+  @IsNumberString()
   phone: string;
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
   email: string;
 }
